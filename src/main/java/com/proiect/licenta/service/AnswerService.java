7ac83b74e66e3d38763bd382c2cf8d4b1acee085
Gabi Lapadat <lapadatrobert123@gmail.com>
2021-11-08 02:52:45 +0200

    Answer and Question service.

--- src/main/java/com/proiect/licenta/service/AnswerService.java
+++ src/main/java/com/proiect/licenta/service/AnswerService.java
@@ -1,7 +1,67 @@
 package com.proiect.licenta.service;
 
+import com.proiect.licenta.exception.ResourceNotFoundException;
+import com.proiect.licenta.model.Answer;
+import com.proiect.licenta.repository.AnswerRepository;
+import com.proiect.licenta.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AnswerService {
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    public Answer createAnswer(Long questionId, Answer answer) {
+
+        answer.setQuestion(questionRepository.getById(questionId));
+
+        return answerRepository.save(answer);
+    }
+
+    public Answer getById(Long answerId) {
+
+        Answer answer = answerRepository.getById(answerId);
+
+        if (answer == null) {
+
+            throw new ResourceNotFoundException(String.format("Answer with id: %d not found!", answerId));
+        }
+
+        return answer;
+    }
+
+    public List<Answer> findAll() {
+
+        return answerRepository.findAll();
+    }
+
+    public Answer updateAnswer(Long answerId, Answer answer) {
+
+        var actualAnswer = answerRepository.getById(answerId);
+
+        actualAnswer.setAnswerText(answer.getAnswerText());
+        actualAnswer.setCorrect(answer.isCorrect());
+
+        return answerRepository.save(actualAnswer);
+    }
+
+    public boolean deleteAnswer(Long answerId) {
+
+        var answer = questionRepository.findById(answerId);
+
+        if (answer.isEmpty()) {
+            return false;
+        }
+
+        questionRepository.deleteById(answerId);
+
+        return true;
+    }
 }
--- src/main/java/com/proiect/licenta/service/QuestionService.java
+++ src/main/java/com/proiect/licenta/service/QuestionService.java
@@ -1,7 +1,67 @@
 package com.proiect.licenta.service;
 
+import com.proiect.licenta.exception.ResourceNotFoundException;
+import com.proiect.licenta.model.Question;
+import com.proiect.licenta.repository.QuestionRepository;
+import com.proiect.licenta.repository.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QuestionService {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private TestRepository testRepository;
+
+    public Question createQuestion(Long testId, Question question) {
+
+        question.setTest(testRepository.getById(testId));
+
+        return questionRepository.save(question);
+    }
+
+    public Question getById(Long questionId) {
+
+        Question question = questionRepository.getById(questionId);
+
+        if (question == null) {
+
+            throw new ResourceNotFoundException(String.format("Question with id: %d not found!", questionId));
+        }
+
+        return question;
+    }
+
+    public List<Question> findAll() {
+
+        return questionRepository.findAll();
+    }
+
+    public Question updateQuestion(Long questionId, Question question) {
+
+        var actualQuestion = questionRepository.getById(questionId);
+
+        actualQuestion.setQuestionText(question.getQuestionText());
+        actualQuestion.setHasMultipleAnswers(question.isHasMultipleAnswers());
+
+        return questionRepository.save(actualQuestion);
+    }
+
+    public boolean deleteQuestion(Long questionId) {
+
+        var question = questionRepository.findById(questionId);
+
+        if (question.isEmpty()) {
+            return false;
+        }
+
+        questionRepository.deleteById(questionId);
+
+        return true;
+    }
 }
